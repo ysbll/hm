f36c171dfd176da68eaec4c33cdb328c6edb0ac7
ysbll <izabela.m.kwiatkowska@gmail.com>
2020-10-18 21:18:36 +0200

    Package refactoring

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,5 @@
 package tests;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import drivers.DriverManager;
 import drivers.DriverUtils;
 import org.apache.commons.io.FileUtils;
